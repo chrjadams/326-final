da9211a00f4f1b0e9ab455d0537b6d12111d03f1
Chris Adams <73072189+chrjadams@users.noreply.github.com>
2023-05-02 18:48:18 -0400

    Add files via upload

---
+++ final proj/script.js
@@ -0,0 +1,91 @@
+const jobForm = document.getElementById("job-form");
+const jobTableBody = document.getElementById("job-table-body");
+const sortByNameBtn = document.getElementById("sort-by-name-btn");
+const sortByDateBtn = document.getElementById("sort-by-date-btn");
+
+let jobs = [];
+
+jobForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const jobName = document.getElementById("job-name").value;
+  const companyTitle = document.getElementById("company-title").value;
+  const dateApplied = document.getElementById("date-applied").value;
+  const notes = document.getElementById("notes").value;
+
+  const job = {
+    jobName,
+    companyTitle,
+    dateApplied,
+    notes,
+  };
+
+  jobs.push(job);
+
+  displayJobs();
+
+  jobForm.reset();
+});
+
+function displayJobs() {
+  // Clear the table body first
+  jobTableBody.innerHTML = "";
+
+  // Filter the jobs array to remove any empty entries
+  const filteredJobs = jobs.filter((job) => {
+    return job.jobName.trim() !== "" &&
+           job.companyTitle.trim() !== "" &&
+           job.dateApplied.trim() !== "" &&
+           job.notes.trim() !== "";
+  });
+
+  filteredJobs.forEach((job, index) => {
+    const row = document.createElement("tr");
+
+    const jobNameCell = document.createElement("td");
+    jobNameCell.innerText = job.jobName;
+    row.appendChild(jobNameCell);
+
+    const companyTitleCell = document.createElement("td");
+    companyTitleCell.innerText = job.companyTitle;
+    row.appendChild(companyTitleCell);
+
+    const dateAppliedCell = document.createElement("td");
+    dateAppliedCell.innerText = job.dateApplied;
+    row.appendChild(dateAppliedCell);
+
+    const notesCell = document.createElement("td");
+    notesCell.innerText = job.notes;
+    row.appendChild(notesCell);
+
+    const actionCell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => {
+      jobs.splice(index, 1);
+      displayJobs();
+    });
+    actionCell.appendChild(deleteButton);
+    row.appendChild(actionCell);
+
+    jobTableBody.appendChild(row);
+  });
+}
+
+
+
+
+
+
+function sortByName() {
+  jobs.sort((a, b) => a.jobName.localeCompare(b.jobName));
+  displayJobs();
+}
+
+function sortByDate() {
+  jobs.sort((a, b) => new Date(a.dateApplied) - new Date(b.dateApplied));
+  displayJobs();
+}
+
+sortByNameBtn.addEventListener("click", sortByName);
+sortByDateBtn.addEventListener("click", sortByDate);
